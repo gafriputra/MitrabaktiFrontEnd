64f8f1b615e84d090531180dae3741aaa59c65f9
gafri putra <gafri.putra@gmail.com>
2020-04-19 11:02:59 +0700

    Config Corousel

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,56 @@
 <template>
   <div class="home">
     <HeaderMitrabakti />
+    <!-- Hero Section Begin -->
+    <section class="hero-section">
+      <carousel class="hero-items">
+        <div class="single-hero-items set-bg" data-setbg="img/hero-1.jpg">
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-5">
+                <span>Bag,kids</span>
+                <h1>Black friday</h1>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore
+                </p>
+                <a href="#" class="primary-btn">Shop Now</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="single-hero-items set-bg" data-setbg="img/hero-2.jpg">
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-5">
+                <span>Bag,kids</span>
+                <h1>Black friday</h1>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore
+                </p>
+                <a href="#" class="primary-btn">Shop Now</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </carousel>
+    </section>
+    <!-- Hero Section End -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HeaderMitrabakti from "@/components/HeaderMitrabakti.vue";
+// import banner slider owl carousel
+import carousel from "vue-owl-carousel";
 
 export default {
   name: "Home",
   components: {
-    HeaderMitrabakti
-  }
+    HeaderMitrabakti,
+    carousel,
+  },
 };
 </script>
