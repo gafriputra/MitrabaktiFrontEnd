61db6a1f35bbc4c291d32c0bb6845d094d8e106b
gafri putra <gafri.putra@gmail.com>
2020-04-19 11:22:15 +0700

    Create Hero Components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,56 +1,21 @@
 <template>
   <div class="home">
     <HeaderMitrabakti />
-    <!-- Hero Section Begin -->
-    <section class="hero-section">
-      <carousel class="hero-items" :items="1" :autoplay="true" :nav="false">
-        <div class="single-hero-items set-bg" data-setbg="img/hero-1.jpg">
-          <div class="container">
-            <div class="row">
-              <div class="col-lg-5">
-                <span>Bag,kids</span>
-                <h1>Black friday</h1>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore
-                </p>
-                <a href="#" class="primary-btn">Shop Now</a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="single-hero-items set-bg" data-setbg="img/hero-2.jpg">
-          <div class="container">
-            <div class="row">
-              <div class="col-lg-5">
-                <span>Bag,kids</span>
-                <h1>Yellow friday</h1>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore
-                </p>
-                <a href="#" class="primary-btn">Shop Now</a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </carousel>
-    </section>
-    <!-- Hero Section End -->
+    <HeroMitrabakti />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HeaderMitrabakti from "@/components/HeaderMitrabakti.vue";
-// import banner slider owl carousel
-import carousel from "vue-owl-carousel";
+
+import HeroMitrabakti from "@/components/HeroMitrabakti.vue";
 
 export default {
   name: "Home",
   components: {
     HeaderMitrabakti,
-    carousel,
+    HeroMitrabakti,
   },
 };
 </script>
