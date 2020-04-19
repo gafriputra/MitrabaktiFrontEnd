f38ab202b8640d162f39d7326ed25c372c70b144
gafri putra <gafri.putra@gmail.com>
2020-04-19 12:27:47 +0700

    Create Produuct Page And Setting Route

---
+++ src/views/Product.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="product">
+    <HeaderMitrabakti />
+    <FooterMitrabakti />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderMitrabakti from "@/components/HeaderMitrabakti.vue";
+import FooterMitrabakti from "@/components/FooterMitrabakti.vue";
+
+export default {
+  name: "Product",
+  components: {
+    HeaderMitrabakti,
+    FooterMitrabakti,
+  },
+};
+</script>
