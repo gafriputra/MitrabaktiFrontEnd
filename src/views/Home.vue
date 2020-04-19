06c75fed12b742efdcabc7f4cb6be4e635bba6cb
gafri putra <gafri.putra@gmail.com>
2020-04-19 11:41:51 +0700

    Create Instagram Component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,15 +3,16 @@
     <HeaderMitrabakti />
     <HeroMitrabakti />
     <HeroProductMitrabakti />
+    <InstagramMitrabakti />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HeaderMitrabakti from "@/components/HeaderMitrabakti.vue";
-
 import HeroMitrabakti from "@/components/HeroMitrabakti.vue";
 import HeroProductMitrabakti from "@/components/HeroProductMitrabakti.vue";
+import InstagramMitrabakti from "@/components/InstagramMitrabakti.vue";
 
 export default {
   name: "Home",
@@ -19,6 +20,7 @@ export default {
     HeaderMitrabakti,
     HeroMitrabakti,
     HeroProductMitrabakti,
+    InstagramMitrabakti,
   },
 };
 </script>
