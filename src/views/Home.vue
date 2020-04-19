f96971106020da0811ef456a8c96041c09a404be
gafri putra <gafri.putra@gmail.com>
2020-04-19 10:54:52 +0700

    Creating component HeaderMitrabakti

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeaderMitrabakti />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderMitrabakti from "@/components/HeaderMitrabakti.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
+    HeaderMitrabakti
   }
-}
+};
 </script>
